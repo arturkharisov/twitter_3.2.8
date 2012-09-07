3e56e8a12677d4badd0434c18aa376882e3625b4
arturkrasnyh <kharisov.a@yandex.ru>
2012-09-07 11:46:38 +0400

    Added solution of exercises ch7

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
   def gravatar_for(user)
-    gravatar_url = Gravatar.new("#{user.email}").image_url
+    gravatar_url = Gravatar.new("#{user.email}").image_url(s: 30)
     image_tag(gravatar_url, alt: user.name, class: 'gravatar')
   end
 end
